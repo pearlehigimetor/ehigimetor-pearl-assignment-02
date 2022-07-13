23eeb5951297bdd8568b261414b219b9efac4167
Pearl Ehigimetor <109207943+pearlehigimetor@users.noreply.github.com>
2022-07-13 08:57:40 +0000

    Completed Challenge - 1 && Challenge -2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+  return arrayOfNumbers.reduce((pV, nV)=> pV + nV);
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +29,9 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.filter(num => num % 2 === 0).length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
